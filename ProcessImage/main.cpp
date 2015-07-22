9c44c98218adc41f44866286cc73fcd8b69380e0
chenww05 <chenww05@gmail.com>
2015-07-21 22:20:01 -0700

    Initial Commit

---
+++ ProcessImage/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProcessImage
+//
+//  Created by Weiwei Chen on 7/21/15.
+//  Copyright © 2015 Weiwei Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
