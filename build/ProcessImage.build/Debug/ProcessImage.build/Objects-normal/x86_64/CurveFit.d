ddf13f18bc9b5f542876e1a4a78c7dc33bc95789
chenww05 <chenww05@gmail.com>
2015-07-30 14:51:05 -0700

    Remove

--- build/ProcessImage.build/Debug/ProcessImage.build/Objects-normal/x86_64/CurveFit.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/weiweich/Documents/M2M/ProcessImage/ProcessImage/ProcessImage/CurveFit.cpp \
-  /Users/weiweich/Documents/M2M/ProcessImage/ProcessImage/ProcessImage/CurveFit.hpp
--- build/ProcessImage.build/Debug/ProcessImage.build/Objects-normal/x86_64/Pixel.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/weiweich/Documents/M2M/ProcessImage/ProcessImage/ProcessImage/Pixel.cpp \
-  /Users/weiweich/Documents/M2M/ProcessImage/ProcessImage/ProcessImage/Pixel.hpp
